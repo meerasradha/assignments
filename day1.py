92106813fc7470bad04eba898b03bf804cc4d03f
meerasradha <86677392+meerasradha@users.noreply.github.com>
2021-06-29 21:49:01 +0530

    Add files via upload
    day 1 python assignment

---
+++ day1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 21:42:23 2021
+
+@author: meera
+"""
+
+a=int(input("enter 1st num:"))
+b=int(input("enter 2nd num:"))
+c=a-b
+print("sub",c)
+d=a*b
+print("mul",d)
+e=a/b
+print("divide",e)
