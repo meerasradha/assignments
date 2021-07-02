75fb2a37c16ebdad1a746b95244eff2fd29487b9
meerasradha <86677392+meerasradha@users.noreply.github.com>
2021-07-02 11:32:49 +0530

    Add files via upload
    day 4 pilot program

---
+++ day4_pilot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 11:07:28 2021
+
+@author: meera
+"""
+x=int(input("enter the aptitude:"))
+y=1000 
+if x<1000:
+    print("land the plane")
+elif x>1000 and x<5000 :
+    print ("come down to 1000ft")
+else :
+    print("go around and try later")
